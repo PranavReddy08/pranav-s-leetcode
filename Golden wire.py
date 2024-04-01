dc25a630dd5bf0c4c66c3127956fc2e6916e17f9
PranavReddy08 <152850319+PranavReddy08@users.noreply.github.com>
2024-04-01 00:09:05 -0800

    Create Golden wire.py

---
+++ Golden wire.py
@@ -0,0 +1,14 @@
+import heapq
+def find_cost(wires):
+    heapq.heapify(wires)
+    tot=0
+    while len(wires)>1:
+        w1=heapq.heappop(wires)
+        w2=heapq.heappop(wires)
+        tot +=(w1+w2)
+        heapq.heappush(wires,(w1+w2))
+        print(wires,tot)
+    return tot
+wires=list(map(int,input().split()))
+ans=find_cost(wires)
+print('ans :',ans)
