92a985048a98b929d298a2c25098a9d84056b2cf
PranavReddy08 <152850319+PranavReddy08@users.noreply.github.com>
2024-04-18 11:08:37 +0530

    Create bottom view.py

---
+++ bottom view.py
@@ -0,0 +1,48 @@
+class newNode:
+	def __init__(self, key):
+		self.data = key
+		self.left = None
+		self.right = None
+		self.hd = 0
+
+def bottomview(root):
+
+	if(root == None):
+		return
+	q = []
+	m = dict()
+	hd = 0
+	root.hd = hd
+
+	q.append(root)
+
+	while(len(q)):
+		root = q[0]
+		hd = root.hd
+		m[hd] = root.data
+		if(root.left):
+			root.left.hd = hd - 1
+			q.append(root.left)
+
+		if(root.right):
+			root.right.hd = hd + 1
+			q.append(root.right)
+
+		q.pop(0)
+	for i in sorted(m):
+		print(m[i], end=" ")
+
+
+# Driver Code
+if __name__ == '__main__':
+
+	root = newNode('A')
+	root.left = newNode('B')
+	root.right = newNode('C')
+	root.left.left = newNode('D')
+	root.left.right = newNode('E')
+	root.right.left = newNode('F')
+	root.right.right = newNode('G')
+	print("The bottom view of the tree is: ")
+	bottomview(root)
+
